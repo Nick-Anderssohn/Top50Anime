05a96986c96ba38ec7c56cccf42fe7a4b3437fb0
Nick Anderssohn <ncadevelopment@gmail.com>
2016-04-26 22:59:34 -0700

    runs through mal's html and finds titles in a very brute force manner

---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	fmt.Println(parseUpToFiftyAnime(50))
+	//parseUpToFiftyAnime(50)
+
+	//test()
+
+}
+
+func getMyAnimeListHTMLIOReader() io.Reader {
+	//get the html code from myanimelist
+	response, err1 := http.Get("http://myanimelist.net/topanime.php?_location=mal_h_m")
+	if nil == err1 {
+		// htmlCode, err2 := ioutil.ReadAll(response.Body)
+		// if nil == err2 {
+		// 	return string(htmlCode)
+		// }
+		// fmt.Println(err2)
+		// return ""
+		return response.Body
+	}
+	fmt.Println(err1)
+	return nil
+}
+
+func getMALHTML() string {
+	response, err := http.Get("http://myanimelist.net/topanime.php?_location=mal_h_m")
+	if nil == err {
+		htmlCode, _ := ioutil.ReadAll(response.Body)
+		return string(htmlCode)
+	}
+	return ""
+}
+
+func parseTop5AnimeHTMLCode() {
+	//var topTitles []string
+	reader := getMyAnimeListHTMLIOReader() //the io.Reader
+
+	tokenizer := html.NewTokenizer(reader) //used to read the html code token by tokenizer
+	for {
+		curToken := tokenizer.Next() //advance to next token
+
+		//if reader is done return
+		if html.ErrorToken == curToken {
+			return
+		}
+
+		//if text is Top Anime, titles could be in following tokens...
+		if "Top Anime" == string(tokenizer.Text()) {
+			keepScanning := true
+			for keepScanning {
+				curToken = tokenizer.Next()
+				//check for end of reader or need to escape this loop...
+				if html.ErrorToken == curToken {
+					return
+				}
+
+				_, hasAttr := tokenizer.TagName() //check if this token has attributes
+				if hasAttr {
+					hadClass := false
+					//iterate through attributes
+					for hasAttr {
+						attrName, attrValue, moreAttr := tokenizer.TagAttr()
+						hasAttr = moreAttr
+						if "class" == string(attrName) && "rank" == string(attrValue) {
+							curToken = tokenizer.Next()
+							curToken = tokenizer.Next()
+							//scan until next token is text..this should be anime titles
+							for "Text" != curToken.String() {
+								curToken = tokenizer.Next()
+							}
+							fmt.Println(string(tokenizer.Text()))
+							hadClass = true
+						}
+					}
+					if !hadClass {
+						keepScanning = false
+					}
+				}
+			}
+		}
+
+	}
+	//return topTitles
+}
+
+//walks through char by char....painfully
+func parseUpToFiftyAnime(num int) string {
+	s := "<img alt=\"Anime: "
+	var listOfShows string
+	//var listOfIndecies []int //for testing
+	htmlCode := getMALHTML()
+	curSlice := htmlCode[0:]
+	for i := 0; i < num; i++ {
+		//find next instance of img alt
+		index := strings.Index(htmlCode, s)
+		//listOfIndecies = append(listOfIndecies, index)
+		listOfShows += subStr(htmlCode, index+17, "\""[0]) + "\n"
+		htmlCode = string(curSlice[index+17:])
+		curSlice = htmlCode[0:]
+	}
+	return listOfShows
+}
+
+func subStr(s string, startIndex int, endChar byte) string {
+	var i int
+	for i = startIndex; s[i] != endChar; i++ {
+	}
+	return string(s[startIndex:i])
+}
+
+// func test() {
+// 	s := "This is a test"
+// 	i := strings.Index(s, "is a")
+// 	slice := s[i:4]
+// 	fmt.Println(s)
+// 	fmt.Println(string(slice))
+//
+// }
